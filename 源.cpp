da5470c9b907e0e6d28b1b4a09c7eb0990e285dc
LeslieYang6 <40122410+LeslieYang6@users.noreply.github.com>
2018-08-30 16:27:34 +0800

    Add files via upload

--- 源.cpp
+++ 源.cpp
@@ -1,167 +1,165 @@
-#include<iostream>
+// C program to demonstrate delete operation in binary search tree
+#include<stdio.h>
+#include<stdlib.h>
 
-using namespace std;
-
-struct Node
+struct node
 {
 	int key;
-	Node *left=NULL;
-	Node *right=NULL;
+	int subtree_size;
+	struct node *left, *right;
 };
 
-class Binary_Search_Tree
+// A utility function to create a new BST node
+struct node *newNode(int item)
 {
-private:
-	Node *pre;
-public:
-	Binary_Search_Tree()
-	{   
-		pre = new Node;
-		pre->key = INT_MAX;
-	}
-	void Insert(int x);
-	bool Delete(int x);
-	void Sort_Display(Node *p);
-	Node *getroot()
-	{
-		return pre->left;
-	}
+	struct node *temp = (struct node *)malloc(sizeof(struct node));
+	temp->subtree_size = 1;
+	temp->key = item;
+	temp->left = temp->right = NULL;
+	return temp;
+}
 
-};
-
-void Binary_Search_Tree::Insert(int x)
+// A utility function to do inorder traversal of BST
+void inorder(struct node *root)
 {
-	Node *head = pre;
-	Node *parent=pre;
-	while (head!=NULL)
+	if (root != NULL)
 	{
-		if (x >= head->key)
-		{   
-			parent = head;
-			head = head->right;
-		}
-		else
-		{   
-			parent = head;
-			head = head->left;
-		}
+		inorder(root->left);
+		printf("%d ", root->key);
+		inorder(root->right);
 	}
-	if (parent->key < x)
+}
+
+/* A utility function to insert a new node with given key in BST */
+struct node* insert(struct node* node, int key)
+{
+	/* If the tree is empty, return a new node */
+	if (node == NULL) return newNode(key);
+
+	/* Otherwise, recur down the tree */
+	if (key < node->key)
 	{
-		parent->right = new Node;
-		parent->right->key = x;
+		node->left = insert(node->left, key);
+		node->subtree_size++;
 	}
 	else
 	{
-		parent->left = new Node;
-		parent->left->key = x;
+		node->right = insert(node->right, key);
+		node->subtree_size++;
 	}
 
-   
-	
+	/* return the (unchanged) node pointer */
+	return node;
 }
 
-bool Binary_Search_Tree::Delete(int x)
+/* Given a non-empty binary search tree, return the node with minimum
+key value found in that tree. Note that the entire tree does not
+need to be searched. */
+struct node * minValueNode(struct node* node)
 {
-	Node *head = pre;
-	Node *parent=NULL;
-	while (head!=NULL&&head->key != x)
-	{
-		if (head->key < x)
-		{   
-			parent = head;
-			head = head->right;
-		}
-		else
-		{   
-			parent = head;
-			head = head->left;
-		}
-	}
+	struct node* current = node;
 
-	if (head == NULL)
-	{
-		cout << "not found";
-		return 0;
-	}
+	/* loop down to find the leftmost leaf */
+	while (current->left != NULL)
+		current = current->left;
 
-	if (head->left == NULL && head->right == NULL)
-	{
-		if (parent->left == head)
-			parent->left = NULL;
-		else
-			parent->right = NULL;
-		delete head;
-		return 1;
-	}
-    
-	if (head->left == NULL&&head->right!=NULL)
-	{
-		if (parent->left == head)
-			parent->left = head->right;
-		else
-			parent->right = head->right;
-		delete head;
-		return 1;
-	}
-   
-	if (head->right == NULL&&head->left!=NULL)
-	{
-		if (parent->left == head)
-			parent->left = head->left;
-		else
-			parent->right = head->left;
-		delete head;
-		return 1;
-	}
-	if (head->right != NULL && head->left != NULL)
-	{
-		if (parent->left == head)
-		{
-			parent->left = head->right;
-			parent = head->right;
-			while (parent->left != NULL)
-				parent = parent->left;
-			parent->left = head->left;
-			delete head;
-		}
-		else
-		{
-			parent->right = head->left;
-			parent = head->left;
-			while (parent->left != NULL)
-				parent = parent->right;
-			parent->right = head->right;
-			delete head;
-		}
-		return 1;
-	}
+	return current;
 }
 
-void Binary_Search_Tree::Sort_Display(Node *p)
-{   
-	if (p->left != NULL)
-		Sort_Display(p->left);
-	cout << p->key << " ";
-	if (p->right != NULL)
-		Sort_Display(p->right);
+/* Given a binary search tree and a key, this function deletes the key
+and returns the new root */
+struct node* deleteNode(struct node* root, int key)
+{
+	// base case
+	if (root == NULL) return root;
+
+	// If the key to be deleted is smaller than the root's key,
+	// then it lies in left subtree
+	if (key < root->key)
+	{
+		root->left = deleteNode(root->left, key);
+		root->subtree_size--;
+	}
+
+	// If the key to be deleted is greater than the root's key,
+	// then it lies in right subtree
+	else if (key > root->key)
+	{
+		root->right = deleteNode(root->right, key);
+		root->subtree_size--;
+	}
+	// if key is same as root's key, then This is the node
+	// to be deleted
+	else
+	{
+		// node with only one child or no child
+		if (root->left == NULL)
+		{
+			struct node *temp = root->right;
+			free(root);
+			return temp;
+		}
+		else if (root->right == NULL)
+		{
+			struct node *temp = root->left;
+			free(root);
+			return temp;
+		}
+
+		// node with two children: Get the inorder successor (smallest
+		// in the right subtree)
+		struct node* temp = minValueNode(root->right);
+
+		// Copy the inorder successor's content to this node
+		root->key = temp->key;
+
+		// Delete the inorder successor
+		root->right = deleteNode(root->right, temp->key);
+		root->subtree_size--;
+	}
+	return root;
 }
 
+int GetLand(node *root, int key)
+{
+	int land = 0;
+	while (root->key != key)
+	{
+		if (root->key < key)
+		{
+			land += 1 + root->left->subtree_size;
+			root = root->right;
+		}
+		else
+		{
+			root = root->left;
+		}
+	}
+	if (root->left != NULL)
+		land += root->left->subtree_size + 1;
+	else
+		land += 1;
+	return land;
+}
 
-
+// Driver Program to test above functions
 int main()
 {
-	Binary_Search_Tree T;
-	T.Insert(8);
-	T.Insert(15);
-	T.Insert(200);
-	T.Insert(9);
-	T.Insert(5);
-	T.Insert(65);
-	T.Insert(4);
-	T.Insert(-65);
-	T.Insert(3);
-	T.Delete(4);
-	T.Sort_Display(T.getroot());
+	/* Let us create following BST
+	50
+	/     \
+	30      70
+	/  \    /  \
+	20   40  60   80 */
+	struct node *root = NULL;
+	root = insert(root, 50);
+	root = insert(root, 30);
+	root = insert(root, 20);
+	root = insert(root, 40);
+	root = insert(root, 70);
+	root = insert(root, 60);
+	root = insert(root, 80);
+	printf("%d", GetLand(root,50));
 	system("pause");
+	return 0;
 }
